b45f58dbb49b397702abf939e7602cf87bf8a305
prithvikings <prithvi07raj07@gmail.com>
2024-06-17 23:33:47 +0530

    object added

---
+++ Javascript/object/script.js
@@ -0,0 +1,18 @@
+var obj={};
+var obj2=new Object();
+
+var obj3={
+    name:"prithvi",
+    age:18,
+    email:"kuch@kuch.com",
+    contact:7672812803
+};
+
+var SmartPhone={
+    name:"Samsung F41",
+    battery:"4500mah",
+    camera:"24mpx",
+    ImeiNo:7672812803,
+    ScreenSize:6.4,
+    Color:"NavyBlue"
+};
