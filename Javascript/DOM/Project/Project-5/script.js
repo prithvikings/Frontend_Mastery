86af04b6502e81b19c2f351298d0761884fedcad
prithvikings <prithvi07raj07@gmail.com>
2024-06-27 02:22:20 +0530

    Tailwind_Learning

--- Javascript/DOM/Project/Project-5/script.js
+++ Javascript/DOM/Project/Project-5/script.js
@@ -3,7 +3,21 @@ let main=document.querySelector("#main");
 
 let cursor=document.querySelector("#cursor");
 
-main.addEventListener("mousemove",(dets)=>{
-    cursor.style.left=dets.x+"px";
-    cursor.style.top=dets.y+"px";
-})
+main.addEventListener("mousemove", (dets) => {
+    let x = dets.clientX;
+    let y = dets.clientY;
+
+    // Calculate the boundaries
+    let crWidth = cursor.offsetWidth;
+    let crHeight = cursor.offsetHeight;
+    let maxX = window.innerWidth - crWidth;
+    let maxY = window.innerHeight - crHeight;
+
+    // Constrain the position
+    if (x > maxX) x = maxX;
+    if (y > maxY) y = maxY;
+
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+    console.log(dets);
+});
