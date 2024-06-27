f2b89b5ce6eadce3011b516888a0b881d6ee1ecf
prithvikings <prithvi07raj07@gmail.com>
2024-06-27 12:02:18 +0530

    .

--- MiniProject/Random_Password_Generator/script.js
+++ MiniProject/Random_Password_Generator/script.js
@@ -1,27 +1,28 @@
-const passwordBox=document.getElementById("Password");
-const length=12;
-const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase="abcdefghijklmnopqrstuvwxyz";
-let button=document.querySelector("button");
-button.addEventListener("click",()=>{
-    createPassword();
-})
-const number='1234576890';
-const symbol='@$';
-const allChar=upperCase+lowerCase+number+symbol;
-const createPassword=()=>{
-    let Password="";
-    Password+=upperCase[Math.floor(Math.random()* upperCase.length)];
-    Password+=lowerCase[Math.floor(Math.random()* lowerCase.length)];
-    Password+=number[Math.floor(Math.random()* number.length)];
-    Password+=symbol[Math.floor(Math.random()* symbol.length)];
-    while(length>Password.length){
-    Password+=allChar[Math.floor(Math.random()* allChar.length)]; 
+let uppercase = "ABCDEFGHJIKLMNOPQRSTUVWXYZ";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let number = "123456789";
+let special = "@$_";
+let passwordbox = document.querySelector("#Password");
+let button = document.querySelector("button");
+let icon = document.querySelector("i");
+let allchar = uppercase + lowercase + special + number;
+let size = 12;
+
+button.addEventListener("click", () => {
+    createpass();
+});
+const createpass=()=>{
+    let pass="";
+    pass+=uppercase[Math.floor(Math.random()*uppercase.length)];
+    pass+=lowercase[Math.floor(Math.random()*lowercase.length)];
+    pass+=number[Math.floor(Math.random()*number.length)];
+    pass+=special[Math.floor(Math.random()*special.length)];
+    while(size>pass.length){
+        pass+=allchar[Math.floor(Math.random()*allchar.length)];
     }
-    passwordBox.value=Password;
+    passwordbox.value=pass;
 }
-const icon=document.querySelector(".ri-file-copy-line");
 icon.addEventListener("click",()=>{
-    Password.select();
-    document.execCommand('copy');
+    passwordbox.select();
+    document.execCommand("copy");
 })
