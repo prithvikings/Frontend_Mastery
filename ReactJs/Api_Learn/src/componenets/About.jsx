eb9acca13349cf5270f473ccf56be191ce3e8772
prithvikings <prithvi07raj07@gmail.com>
2024-08-31 00:01:53 +0530

    .

--- ReactJs/Api_Learn/src/componenets/About.jsx
+++ ReactJs/Api_Learn/src/componenets/About.jsx
@@ -1,7 +1,25 @@
 import React, { useEffect } from 'react'
+import axios from 'axios';
 
 function About() {
+
+
+  const getusers = () => {
+    const api = "https://fakestoreapi.com/users";
+
+    axios.get(api).then(response => {
+      console.log(response);
+      // setProducts(response.data);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+
+
+
     useEffect(()=>{
+      getusers();
       console.log("About page loaded");
 
       return ()=>{
