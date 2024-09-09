f72ae226ccd6590f67c09b1f209b9104e73a59db
prithvikings <prithvi07raj07@gmail.com>
2024-09-10 01:47:38 +0530

    done with eco

--- Ecoband/script.js
+++ Ecoband/script.js
@@ -33,3 +33,18 @@ document.querySelector(".ser-4").addEventListener("mousemove",()=>{
 document.querySelector(".ser-4").addEventListener("mouseleave",()=>{
     document.querySelector(".h-div4").style.top = "100%";
 })
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger: ".part-2",
+    start:"50% 50%",
+    end:"100% 100%",
+    scrub:true,
+    // markers: true,
+}});
+tl
+.to(".banner-1",{
+    left: "-20%",
+}, 'a')
+.to(".banner-2",{
+    right: "-15%",
+}, 'a')
