0a709ab709db4b68d4da877d4ebc2f863d26fdda
prithvikings <prithvi07raj07@gmail.com>
2024-09-10 01:19:43 +0530

    completed till services

---
+++ Ecoband/script.js
@@ -0,0 +1,35 @@
+document.querySelector(".ser-1").addEventListener("mousemove",()=>{
+    document.querySelector(".h-div1").style.top = "0%";
+    document.querySelector(".ser-1").style.cursor="pointer";
+})
+
+document.querySelector(".ser-1").addEventListener("mouseleave",()=>{
+    document.querySelector(".h-div1").style.top = "100%";
+})
+
+document.querySelector(".ser-2").addEventListener("mousemove",()=>{
+    document.querySelector(".h-div2").style.top = "0%";
+    document.querySelector(".ser-2").style.cursor="pointer";
+})
+
+document.querySelector(".ser-2").addEventListener("mouseleave",()=>{
+    document.querySelector(".h-div2").style.top = "100%";
+})
+
+document.querySelector(".ser-3").addEventListener("mousemove",()=>{
+    document.querySelector(".h-div3").style.top = "0%";
+    document.querySelector(".ser-3").style.cursor="pointer";
+})
+
+document.querySelector(".ser-3").addEventListener("mouseleave",()=>{
+    document.querySelector(".h-div3").style.top = "100%";
+})
+
+document.querySelector(".ser-4").addEventListener("mousemove",()=>{
+    document.querySelector(".h-div4").style.top = "0%";
+    document.querySelector(".ser-4").style.cursor="pointer";
+})
+
+document.querySelector(".ser-4").addEventListener("mouseleave",()=>{
+    document.querySelector(".h-div4").style.top = "100%";
+})
