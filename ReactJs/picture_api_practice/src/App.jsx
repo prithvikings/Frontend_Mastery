18c42afa02d577d4809b0d330d6a896a8145b531
prithvikings <prithvi07raj07@gmail.com>
2024-10-19 17:08:28 +0530

    ..

--- ReactJs/picture_api_practice/src/App.jsx
+++ ReactJs/picture_api_practice/src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
     setImages(response.data)
   }
 
+  // useEffect(()=>{
+  //   getImages();
+  // },[])
+
   return (
     <div className='min-h-screen bg-gray-500 p-8'>
       <h1 className='text-3xl font-bold text-center mb-8'>Image Gallery</h1>
