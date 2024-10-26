7dca641b9d9019e66a0567c2a38c6e6dd066b73f
prithvikings <prithvi07raj07@gmail.com>
2024-10-26 23:31:52 +0530

    Revising concept

--- ReactJs/bg-changer/src/App.jsx
+++ ReactJs/bg-changer/src/App.jsx
@@ -12,62 +12,16 @@ function App() {
   const [color, setColor] = useState("#333");
   return (
     <>
-      <div
-        className="h-screen w-full flex justify-center items-end "
-        style={{ backgroundColor: color }}
-      >
-        <div className="w-8/12 h-14 rounded-xl bg-slate-100 mb-6 flex justify-around items-center shadow-2xl border-2 border-black">
-          <button
-            className="bg-red-700 h-10 w-20 rounded-lg text-xl font-custom text-white"
-            onClick={() => {
-              setColor("red");
-            }}
-          >
-            Red
-          </button>
-          <button
-            className="bg-yellow-300 h-10 w-20 rounded-lg text-xl font-custom text-black" style={{ backgroundColor: "Yellow" }}
-            onClick={() => {
-              setColor("yellow");
-            }}
-          >
-            Yellow
-          </button>
-          <button
-            className="bg-blue-700 h-10 w-20 rounded-lg text-xl font-custom text-white"
-            onClick={() => {
-              setColor("blue");
-            }}
-          >
-            Blue
-          </button>
-          <button
-            className="bg-green-600 h-10 w-20 rounded-lg text-xl font-custom text-white"
-            onClick={() => {
-              setColor("green");
-            }}
-          >
-            Green
-          </button>
-          <button
-            className="bg-orange-400 h-10 w-20 rounded-lg text-xl font-custom text-white"
-            onClick={() => {
-              setColor("orange");
-            }}
-          >
-            orange
-          </button>
-
-          <button
-            className="bg-cyan-400 h-10 w-20 rounded-lg text-xl font-custom text-black "
-            onClick={() => {
-              setColor("cyan");
-            }}
-          >
-            Cyan
-          </button>
-        </div>
+    <div className="h-screen w-full flex justify-center items-end " style={{ backgroundColor: color }}>
+      <div className="w-8/12 h-14 rounded-xl bg-slate-100 mb-6 flex justify-around items-center shadow-2xl border-2 border-black">
+        <button className="bg-red-700 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("red")}}>Red</button>
+        <button className="bg-blue-700 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("blue")}}>Blue</button>
+        <button className="bg-green-700 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("green")}}>Green</button>
+        <button className="bg-orange-500 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("orange")}}>Orange</button>
+        <button className="bg-cyan-400 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("cyan")}}>Cyan</button>
+        <button className="bg-yellow-400 h-10 w-24 rounded-lg text-xl font-custom text-white" onClick={()=>{setColor("yellow")}}>Yellow</button>
       </div>
+    </div>
     </>
   );
 }
--- ReactJs/stateapp/stateapp/src/Conditional.jsx
+++ ReactJs/stateapp/stateapp/src/Conditional.jsx
@@ -5,7 +5,7 @@ function Conditional() {
   return (
     <>
     <div className='bg-gray-700 w-full h-screen flex justify-center items-center flex-col'>
-        <h1 className={`text-4xl font-bold ${a===false?"text-yellow-300 ":"text-white"}`}>{a===false?"Hello":"Bye"}</h1>
+      <h1 className={`text-4xl font-bold ${a==false?"text-yellow-300":"text-white"}`}>{a==false?"Hello":"Bye"}</h1>
         <button onClick={()=>{b(!a)}} className='py-2 px-4 bg-green-700 text-white text-2xl rounded-lg mt-5'>change</button>
     </div>
     </>
