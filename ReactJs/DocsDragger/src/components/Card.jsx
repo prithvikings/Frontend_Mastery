4e5bdeaa5a02a0052a1f25583368a3819ba071eb
prithvikings <prithvi07raj07@gmail.com>
2024-11-18 00:51:17 +0530

    DocDragger Project using framer motion

--- ReactJs/DocsDragger/src/components/Card.jsx
+++ ReactJs/DocsDragger/src/components/Card.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import {motion} from "framer-motion";
 
-function Card() {
+function Card({ data,refrence }) {
   return (
-    <div>
-        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90 '></div>
-    </div>
-  )
+    <motion.div drag dragConstraints={refrence} whileDrag={{scale:1.1}} dragElastic={.1} dragTransition={{bounceStiffness:100, bounceDamping:30}} className="flex-shrink-0 relative w-56 h-64 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden">
+      <FaRegFileAlt />
+      <p className=" text-sm mt-5 font-semibold  leading-tight">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex items-center py-3 justify-between px-8  mb-3">
+          <h5>{data.filesize}</h5>
+          <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+            {data.close ? (
+              <IoClose size=".7em" color="#fff" />
+            ) : (
+              <LuDownload size=".7em" color="#fff" />
+            )}
+          </span>
+        </div>
+        {data.tag.isopen && (
+          <div className={`tag flex items-center justify-center ${data.tag.tagcolor === "green" ? "bg-green-600" : "bg-blue-600"} w-full py-4 h-10`}>
+          <h3 className="text-sm font-semibold">{data.tag.tagTitile}</h3>
+        </div>
+          )}
+      </div>
+    </motion.div>
+  );
 }
 
-export default Card
+export default Card;
--- ReactJs/DocsDragger/src/components/Foreground.jsx
+++ ReactJs/DocsDragger/src/components/Foreground.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Card from './Card'
+import React, { useRef, useState } from "react";
+import Card from "./Card";
+
 
 function Foreground() {
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque!",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isopen: true,
+        tagTitile: "Download Now",
+        tagcolor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque!",
+      filesize: ".9mb",
+      close: false,
+      tag: {
+        isopen: true,
+        tagTitile: "Upload Now",
+        tagcolor: "blue",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque!",
+      filesize: ".9mb",
+      close: true,
+      tag: {
+        isopen: false,
+        tagTitile: "Download Now",
+        tagcolor: "blue",
+      },
+    },
+  ];
+  const ref=useRef(null);
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-        <Card />
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-8 flex-wrap p-5 ">
+      {data.map((item, index) => {
+        return <Card data={item} key={index}  refrence={ref}/>;
+      })}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
